581229546a2c827b8e09186a79bb838c3edf4927
Christian Huettig <chhu79@gmail.com>
2017-04-28 17:29:12 +0200

    Testing for efficient Poisson solver...

--- tests/test9.cpp
+++ tests/test9.cpp
@@ -1,14 +1,43 @@
 #include "includes.hpp"
 #include "solver.hpp"
 
+// Dimension setup
 
-template <typename DTYPE, typename HCSTYPE>
-class PField: public Field<DTYPE, HCSTYPE> {
-	void poisson(Field<DTYPE, HCSTYPE> &b) {
 
+void poisson(ScalarField1 &x, ScalarField1 &b) {
+	b.propagate();
+	level_t highest = x.getHighestLevel();
+	H1 &h =x.hcs;
+	data_t div = x.nElementsTop();
+
+	ScalarField1 fake;	// easy way to obtain correct coeffs.
+
+	// zero special since there is no "parent"
+	data_t l_val = x.get(h.getNeighbor(0, 1), true);
+	data_t r_val = x.get(h.getNeighbor(0, 0), true);
+	x[0] = 0.5 * l_val + 0.5 * r_val + b[0]/4;
+	for (level_t l = 1; l <= highest; l++) {
+
+		for (auto it = x.begin(false, l); it != x.end(); ++it) {
+			cout << x.hcs.toString((*it).first);
+			coord_t coord = (*it).first;
+			data_t &value = (*it).second;
+			//value = 0;
+			data_t b_delta = 0;// / (l + 4);// / (1 );
+			ScalarField1::coeff_map_t coeffs;
+			fake.getCoeffs(coord, coeffs, false);
+			for (auto e : coeffs) {
+				value += x.get(e.first, true) * e.second;
+				b_delta += b.get(e.first, true) * e.second;
+				//cout << "[ " << h.toString(e.first) << " :: " << e.second << " ]";
+			}
+			//b[coord] = b_delta;// + b.get(coord, true) / (l + 2);
+			cout << " VAL: " << value <<  " B DELTA: " << b_delta << endl;
+		}
+		fake.clear();
+		fake.createEntireLevel(l);
 	}
-};
-
+}
 
 int main(int argc, char **argv) {
 
@@ -19,20 +48,30 @@ int main(int argc, char **argv) {
 	#endif
 
 
-
-	// Dimension setup
-	typedef H1 HCS;
-	typedef PField<data_t, H1> ScalarField;
+	H1 h;
 
 	// Resolution setup
-	const int max_level = 9;	// Max level for "C" field
+	const int max_level = 3;	// Max level for "C" field
 
-	HCS hcs;
 
-	ScalarField c;
-	c.createEntireLevel(4);
-	for (auto e : c) e.second = 1;
-	write_txt("test9.txt", c);
+
+	ScalarField1 x, b;
+	b.createEntireLevel(max_level);
+	x.createEntireLevel(max_level);
+
+	//x.boundary[0] = [](ScalarField1 *self, coord_t cc)->data_t { return 1;};
+
+	x = 0;
+	b = 0;
+	b[h.createFromUnscaled(max_level, {4})] = 1;
+	b.boundary[0] = b.boundary[1] = [](ScalarField1 *self, coord_t cc)->data_t { return 0;};
+	b.propagate();
+
+	poisson(x,b);
+	poisson(x,b);
+
+	write_txt("test9.txt", x);
+	write_txt("test9b.txt", b,false);
 
 
 
